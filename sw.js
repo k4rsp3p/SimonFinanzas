836d3330457a5cd22900144080b4db84eb68a70a
k4rsp3p <gstayselect@gmail.com>
2026-03-29 17:17:02 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'simon-v3-' + Date.now();
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    ).then(() => clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
