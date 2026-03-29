398cf7467d2cbc0b8d13df6b6bc15dbfbd29caf9
k4rsp3p <gstayselect@gmail.com>
2026-03-29 17:16:34 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => clients.claim());
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.open('simon-v2').then(cache =>
-    cache.match(e.request).then(r => r || fetch(e.request).then(res => {
-      cache.put(e.request, res.clone()); return res;
-    }))
-  ));
-});
